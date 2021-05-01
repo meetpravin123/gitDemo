2feac87ccfe2e55517d42ef4e5280b3ca638dc21
eypawarpr <pravin.pawar@ey.com>
2021-05-01 17:24:43 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gitDeployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
